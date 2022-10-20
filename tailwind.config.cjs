8c6062f4a06d0940f8bd5c39688091e39f82b786
Estifanos <estifanosbeyene21@gmail.com>
2022-10-20 15:46:14 -0400

    feat: add fonts and tw plugins

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    fontFamily: {
+      dashboard: ["Smythe"],
+      heading: ["Expletus Sans"],
+      body: ["Hind"],
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
 };
