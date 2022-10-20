01e5b32f7c9b96f9062c3d2ca4ff7eb33411b349
Estifanos <estifanosbeyene21@gmail.com>
2022-10-20 11:51:11 -0400

    feat: add basic layout to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,37 @@
-import { useEffect } from "react";
+// NOTE the name of the website is Hermóðr
+// Create layout of the page
+import tw from "tailwind-styled-components";
+
+const SPageLayout = tw.main`
+  max-w-2xl
+  mx-4
+  my-10
+  md:mx-auto
+`;
+
+const Dashboard = () => {
+  return (
+    <div className="flex flex-col gap-5 items-center">
+      {/* TODO: use greek like fonts */}
+      <h1 className="text-4xl md:text-6xl">Talaria</h1>
+      <nav>
+        <ul className="flex gap-8">
+          <li>Home</li>
+          <li>Login</li>
+          <li>Register</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 const App = () => {
-  useEffect(() => {
-    const checkHealth = async () => {
-      let response = await fetch("/api/checkhealth");
-      console.log(response);
-    };
-
-    checkHealth();
-  }, []);
-  return <div>Hello World</div>;
+  return (
+    <SPageLayout>
+      <Dashboard />
+      <h1>Create an account</h1>
+    </SPageLayout>
+  );
 };
 
 export default App;
