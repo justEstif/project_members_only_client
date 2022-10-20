45a8184180d7bfcbd16b529b715e59a318d5c998
Estifanos <estifanosbeyene21@gmail.com>
2022-10-20 16:15:53 -0400

    feat: extract input into separate component

---
+++ src/SInput.tsx
@@ -0,0 +1,17 @@
+import tw from "tailwind-styled-components";
+
+const SInput = tw.input`
+  block
+  px-0.5
+  mt-0
+  w-full
+  border-0
+  border-b-2
+  border-gray-200
+  focus:border-black
+  focus:ring-0
+  font-body
+`;
+
+
+export default SInput
