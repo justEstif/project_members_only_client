1537b816643bd65d1f2fba07fc923d03f0ffaa05
Estifanos <estifanosbeyene21@gmail.com>
2022-10-21 18:24:04 -0400

    feat: add conditional nav bar links

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
+import useStore from "./store";
 
+/**
+ * @description react component that includes the website title and navbar
+ */
 const Dashboard = () => {
+  const auth = useStore((state) => state.auth);
   return (
     <div className="flex flex-col gap-5 justify-center items-center font-dashboard">
       <h1 className="text-4xl md:text-6xl">Talaria</h1>
@@ -10,14 +15,20 @@ const Dashboard = () => {
             <Link to="/">Home</Link>
           </li>
 
-          {/* TODO: Change links based of if there is a user or not */}
-
           <li>
-            <Link to="/login">Login</Link>
+            {!auth ? (
+              <Link to="/login">Login</Link>
+            ) : (
+              <Link to="/account">Account</Link>
+            )}
           </li>
 
           <li>
-            <Link to="/register">Register</Link>
+            {!auth ? (
+              <Link to="/register">Register</Link>
+            ) : (
+              <Link to="/logout">Logout</Link>
+            )}
           </li>
         </ul>
       </nav>
