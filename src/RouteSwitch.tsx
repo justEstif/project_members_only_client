645bd85cedb4f866aad09f9dcf029625bd3227d4
Estifanos <estifanosbeyene21@gmail.com>
2022-10-21 12:54:57 -0400

    feat: replace App with RouteSwitch

--- src/RouteSwitch.tsx
+++ src/RouteSwitch.tsx
@@ -1,11 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./Layout";
+import LoginForm from "./LoginForm";
+import RegisterForm from "./RegisterForm";
 
 const RouteSwitch = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/register" element={<RegisterPage/>} />
+        <Route path="/" element={<Layout />}>
+          <Route path="register" element={<RegisterForm />} />
+          <Route path="login" element={<LoginForm />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
 };
+
+export default RouteSwitch;
