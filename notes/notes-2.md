1810a6f4e7b98fc0099f93889d34b264dda2707b
Estifanos <estifanosbeyene21@gmail.com>
2022-10-23 12:27:03 -0400

    feat: add messages form

--- notes/notes-2.md
+++ notes/notes-2.md
@@ -1,5 +1,9 @@
-1. [ ] Added logout handler
+1. [X] Added logout handler
 2. [ ] Added create message handler
+  - if user is logged in:
+    - show form, and on message submit update page
+  - else
+    - show place holder
 3. [ ] Added account handler
   - [ ] update user role
 4. [ ] Add admin only functions
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,16 +1,71 @@
+import { FieldValues, useForm } from "react-hook-form";
+import useStore from "./store";
+import { SInput } from "./StyledComponents";
+
+/**
+ * @description the messages in the db
+ */
+const Messages = () => {
+  return <div>The messages</div>;
+};
+
+/**
+ * @description the new message form that is only accessible to logged in users
+ */
+const MessageForm = () => {
+  type FormData = {
+    message: string;
+  };
+  const { register, handleSubmit } = useForm<FormData>();
+  // const token = useStore((state) => state.auth?.token);
+  const onSubmit = (values: FieldValues) => {
+    // TODO: call api here
+    console.log(values);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        <div className="flex gap-3">
+          <button type="submit">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+          </button>
+
+          <SInput
+            type="message"
+            placeholder="Enter your message"
+            {...register("message", { required: true })}
+          />
+        </div>
+      </label>
+    </form>
+  );
+};
+
 /**
  * @description home page
  * @returns new message form, and messages
  */
 const Home = () => {
+  const auth = useStore((state) => state.auth);
   return (
     <div className="container my-10 mx-auto font-heading">
-      Add form here
-      <br />
-      Shows messages under
-      <br />
-      Check against the database to get the messages and include token if it
-      exists
+      {/* TODO: Don't show the form if there is no user */}
+      {auth ? <MessageForm /> : <span>Login</span>}
+      <Messages />
     </div>
   );
 };
