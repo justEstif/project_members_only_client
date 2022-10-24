256836d46652569f2209c683a7737601611ee5ea
Estifanos <estifanosbeyene21@gmail.com>
2022-10-24 19:40:57 -0400

    feat: fix user update form

--- src/Account.tsx
+++ src/Account.tsx
@@ -2,11 +2,11 @@ import { FieldValues, useForm } from "react-hook-form";
 import { TUser } from "./types";
 import useStore from "./store";
 import { SInput } from "./StyledComponents";
+
 /**
  * @description account page function component
  */
 const Account = () => {
-  // setup form data
   type FormData = {
     name?: string;
     userName?: string;
@@ -15,54 +15,52 @@ const Account = () => {
     password?: string; // min length 6
     passwordConfirmation?: string;
   };
-  // get token
-  // setup react-hook-form
-  // return form with secret key input
-  // add an onsubmit function to call api
-  // update page on form submit success
-  // update user ??
-  const auth = useStore((state) => state.auth);
-  const {
-    register,
-    handleSubmit,
-    getValues,
-    setError,
-    formState: { errors },
-  } = useForm<FormData>();
+  const { auth, updateUser } = useStore((state) => state);
+  const { register, handleSubmit } = useForm<FormData>();
 
+  /**
+   * @description update the user
+   */
   const onSubmit = async (values: FieldValues) => {
-    if (!auth) return; // TODO: better handle
-    const response = await fetch(`/api/user/${auth.user.id || ""}`, {
+    const response = await fetch(`/api/user/${auth ? auth.user.id : ""}`, {
       method: "PUT",
       headers: {
         Accept: "application/json, text/plain, */*",
         "Content-Type": "application/json",
-        Authorization: `Bearer ${auth.token}`,
+        Authorization: `Bearer ${auth ? auth.token : ""}`,
       },
       body: JSON.stringify(values),
     });
 
     if (response.ok) {
-      const json = await response.json();
-      console.log(json);
-    } else {
-      const error = await response.json().catch((error) => error);
-      console.error(error);
+      const user = (await response.json()) as TUser;
+      if (auth?.token) {
+        updateUser({ user: user, token: auth?.token });
+      } else {
+        const error = await response.json().catch((error) => error);
+        console.error(error);
+      }
     }
+
+    return (
+      <div>
+        <form
+          className="grid grid-cols-1 gap-6"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <label>
+            {auth && auth.user.role === "USER" && (
+              <span className="text-gray-700">Become a member:</span>
+            )}
+            {auth && auth.user.role === "MEMBER" && (
+              <span className="text-gray-700">Become an admin:</span>
+            )}
+            <SInput type="text" {...register("secretKey")} />
+          </label>
+        </form>
+      </div>
+    );
   };
-  return (
-    <div>
-      <form
-        className="grid grid-cols-1 gap-6"
-        onSubmit={handleSubmit(onSubmit)}
-      >
-        <label>
-          <span className="text-gray-700">Become a member:</span>
-          <SInput type="text" {...register("secretKey")} />
-        </label>
-      </form>
-    </div>
-  );
 };
 
 export default Account;
