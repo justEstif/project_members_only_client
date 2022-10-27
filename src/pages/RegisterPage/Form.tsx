543f89f2a115396b38419671524166631f4c3a2d
Estifanos <estifanosbeyene21@gmail.com>
2022-10-27 10:41:25 -0400

    feat: refactoring ui

---
+++ src/pages/RegisterPage/Form.tsx
@@ -0,0 +1,17 @@
+const Form = () => {
+  return (
+    <form>
+      <fieldset>
+        <label>
+          <div>
+            <p>Name</p>
+            <p>Error message</p>
+          </div>
+        </label>
+        <input />
+      </fieldset>
+    </form>
+  );
+};
+
+export default Form;
